df27a8f77aab11d1c58649d9c83768bb81c65594
Andy <andygnewman@gmail.com>
2017-11-05 17:16:03 +0000

    initial api set up

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const getWeather = require('./src/get-weather');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return getWeather(req)
+  .then(weather => res.json({msg: weather}));
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}, yippeeee`));
