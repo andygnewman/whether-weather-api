93d967775b57383f230dbd4bde79cd4ca5b394d3
Andy <andygnewman@gmail.com>
2017-11-06 14:59:14 +0000

    get and format forecast data

--- src/get-weather.js
+++ src/get-weather.js
@@ -1,7 +1,10 @@
 const fetch = require('node-fetch');
 
-const host = 'api.openweathermap.org';
-const appid = process.env.APPID;
+const HOST = 'api.openweathermap.org';
+const APPID = process.env.APPID;
+const AUTH = `appid=${APPID}`;
+const UNITS = 'units=metric';
+const METHOD = 'POST';
 
 const defaultCities = {
   dublin: 2964574,
@@ -12,15 +15,64 @@ const defaultCities = {
 const getCurrentWeather = (cityIds = Object.values(defaultCities)) => {
   const cityIdsString = cityIds.join(',');
   const id = `id=${cityIdsString}`;
-  const units = 'units=metric';
-  const auth = `appid=${appid}`;
-  const url = `http://${host}/data/2.5/group?${id}&${units}&${auth}`;
-  const method = 'POST';
-  return fetch(url, {method})
-    .then(response => response.json());
+  const url = `http://${HOST}/data/2.5/group?${id}&${UNITS}&${AUTH}`;
+  return fetch(url, {method: METHOD})
+    .then(response => response.json())
+    .then(response => {
+      const list = response.list;
+      return list.map(item => {
+        return {
+          location: item.name,
+          current: {
+            temp: item.main.temp
+          }
+        };
+      });
+    });
 };
 
+const getForecastWeather = (cityIds = Object.values(defaultCities)) => {
+  const forecastDates = [];
+  const promises = [];
+  cityIds.map(city => {
+    const promise = () => {
+      const url = `http://${HOST}/data/2.5/forecast?id=${city}&${UNITS}&${AUTH}`;
+      return fetch(url, {method: METHOD})
+        .then(response => {
+          return response.json();
+        })
+        .then(response => {
+          const list = response.list;
+          const location = response.city.name;
+          const temps = []
+          list.map((item, index) => {
+            const forecastDate = new Date(item.dt_txt);
+            if (forecastDate.getHours() === 12) {
+              if (!forecastDates.includes(forecastDate.toString())) {
+                forecastDates.push(forecastDate.toString());
+              }
+              temps.push(item.main.temp);
+            }
+          });
+          return {
+            location,
+            temps
+          };
+        });
+    };
+    promises.push(promise());
+  });
+  return Promise.all(promises)
+    .then(forecasts => {
+      return {
+        forecastDates,
+        forecasts
+      }
+    });
+};
 
 module.exports = (req) => {
-  return getCurrentWeather();
+  return getForecastWeather()
+  .then(forecasts => console.log(forecasts));
+  // return getCurrentWeather();
 };
