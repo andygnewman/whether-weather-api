603863111312303a1b7aa78954217199ce598557
Andy <andygnewman@gmail.com>
2017-11-06 12:28:15 +0000

    inital api call for current weather

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const getWeather = require('./src/get-weather');
 
 const port = process.env.PORT || 3000;
+
 
 const app = express();
 
--- src/get-weather.js
+++ src/get-weather.js
@@ -1,3 +1,26 @@
+const fetch = require('node-fetch');
+
+const host = 'api.openweathermap.org';
+const appid = process.env.APPID;
+
+const defaultCities = {
+  dublin: 2964574,
+  liverpool: 2644210,
+  birmingham: 2655603
+};
+
+const getCurrentWeather = (cityIds = Object.values(defaultCities)) => {
+  const cityIdsString = cityIds.join(',');
+  const id = `id=${cityIdsString}`;
+  const units = 'units=metric';
+  const auth = `appid=${appid}`;
+  const url = `http://${host}/data/2.5/group?${id}&${units}&${auth}`;
+  const method = 'POST';
+  return fetch(url, {method})
+    .then(response => response.json());
+};
+
+
 module.exports = (req) => {
-  return Promise.resolve('Hello World');
+  return getCurrentWeather();
 };
